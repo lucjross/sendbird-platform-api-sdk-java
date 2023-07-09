9892bef43b65b8299dd027f71a35a3e40e28c396
Lucas Ross <lucas.ross@favordelivery.com>
2023-07-08 22:21:52 -0500

    impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.21"
-    application
+    `java-library`
+    id("org.openapi.generator") version "6.4.0"
 }
 
 group = "io.github.lucjross"
@@ -12,18 +13,59 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java.srcDirs("$buildDir/generated/src/main/java")
+    }
+}
+
+java {
+    withSourcesJar()
+}
+
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+    implementation("com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider:2.15.2")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+    implementation("jakarta.annotation:jakarta.annotation-api:2.1.1")
+
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+openApiGenerate {
+    generatorName.set("java")
+    inputSpec.set("${sourceSets.main.get().resources.srcDirs.first()}/openapi.yaml")
+    outputDir.set("$buildDir/generated")
+    apiPackage.set("com.sendbird.platform.v3.api")
+    modelPackage.set("com.sendbird.platform.v3.model")
+    configOptions.putAll(mapOf(
+        "library" to "apache-httpclient",
+        "serializationLibrary" to "jackson",
+        "useJakartaEe" to "true",
+        "documentationProvider" to "none",
+        "openApiNullable" to "false",
+        "annotationLibrary" to "none",
+        "documentationProvider" to "none",
+    ))
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 
-application {
-    mainClass.set("MainKt")
+    jar {
+        manifest {
+            attributes(mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            ))
+        }
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+        dependsOn(openApiGenerate)
+    }
 }
